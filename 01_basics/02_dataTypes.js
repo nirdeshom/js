7a6f53b12c978911de228c8d5a5097ead8274cbd
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-07-23 22:11:36 +0530

    02 data type

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";  // treat all js code as newer version
+
+// alert(3 + 9);  // we are using node js not browser
+
+console.log("Nirdesh");
+
+let name = "nirdesh";
+let age = 21;
+let loggedIn = true;
+
+// Number => 2 to power 53
+// BigInt => n
+// String => ""
+// Boolean => true / false
+// null => standalone value
+// undefined => 
+// Symbol => unique
+
+// Object
+
+console.log(typeof (undefined))  // undefined
+console.log(typeof (null))  // object
