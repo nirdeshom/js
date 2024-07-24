624864bbe1d5b3ec8c708a242b336851fdec7ce2
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-07-24 14:34:10 +0530

    Comparison Operator

---
+++ operators/02_comparison.js
@@ -0,0 +1,15 @@
+let num1 = 3;
+let num2 = 4;
+console.log(num1 > num2);  // false
+console.log(num1 < num2); // true
+
+console.log(2 == 2);  // true
+console.log(2 == "2");  // true
+console.log(2 === "2");  // false
+
+console.log(3 >= 2); // true
+console.log(3 <= 2); // false
+console.log(3 <= 3); // true
+
+console.log(3 != 4); // true
+console.log(3 != 3); // false
