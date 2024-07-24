ecd2cc19949dd71164623109879e70d247c782b6
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-07-24 13:44:08 +0530

    Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "nirdesh";
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+
+// "99" => 33
+// "99a" => NaN (not a number)
+// true => 1 / false => 0
+// null  => 0
+// undefined => NaN
+// "nirdesh" => NaN
+
+let isLoggedIn  = "";
+
+let booleanIsLoggedIn  = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true / 0 => false
+// "" => false
+// "nirdesh" => true
+
+let my_number = 12
+
+let valueInString = String(my_number);
+console.log(valueInString);
