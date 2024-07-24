f4cb941986dedda620cf73325d021d93836861a0
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-07-24 14:33:22 +0530

    Arithmetic operator

---
+++ operators/01_arithmetic.js
@@ -0,0 +1,8 @@
+let num1 = 6;
+let num2 = 2;
+
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+console.log("Remainder:", num1 % num2);
