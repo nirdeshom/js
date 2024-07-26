26aa98a1c10dc9a9029770266256997311c8907c
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-07-26 12:17:41 +0530

    Add Logical Operators

---
+++ operators/04_logical.js
@@ -0,0 +1,11 @@
+let x = 5;
+let y = 4;
+
+// &&(logical AND: both condition true)
+console.log(x > 3 && y > 3);
+
+// ||(logical OR: one condition true)
+console.log(x > 4 || y >5);
+
+let isLogin  = false;  // true
+console.log(!isLogin);
