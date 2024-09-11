dbc21266164b72da5cfb1865d7fdf9aa6cf45ca3
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-09-11 19:31:07 +0530

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,47 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3;
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("Not match month");
+        break;
+}
+
+let name = "Nirdesh"
+switch (name) {
+    case "vimal":
+        console.log("i am vimal");
+        break;
+
+    case "Nirdesh":
+        console.log("i am nirdesh");
+        break;
+    
+    case "omjeet":
+        console.log("i am omjeet");
+        break;
+        
+    default:
+        console.log("name not match");
+        
+}
