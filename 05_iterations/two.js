9c4548cb21904c624314f0f1a93e1d40179c17ae
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-09-13 23:32:59 +0530

    while and do_while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+// while and do_while loop 
+
+let i = 1;
+while(i <= 10){
+    // console.log(`value of i is ${i}`);
+    i = i + 2;
+}
+
+let myArray = ["nirdesh", "satyam", "uchit", "rohit"];
+let arr = 0
+while(arr < myArray.length){
+    // console.log(myArray[arr])
+    // console.log(`value of ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
+let number = 1 // 11
+do {
+    console.log(`value of ${number}`);
+    number++
+} while (number <= 10);
